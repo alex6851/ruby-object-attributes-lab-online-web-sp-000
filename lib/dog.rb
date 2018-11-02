dffb4c13019fa49a4c7942e891361b6b0c03028b
Alexander Baker <alex.baker685@gmail.com>
2018-11-02 20:19:09 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,20 +1,20 @@
 class Dog
 
-def name=(dogs_name)
-  @name = dogs_name
-end
-
-def name
-  @name
+  def name=(dogs_name)
+    @name = dogs_name
   end
-end
+
+  def name
+    @name
+    end
+  end
 
 
-def breed=(dogs_breed)
-  @breed = dogs_breed
-end
+  def breed=(dogs_breed)
+    @breed = dogs_breed
+  end
 
-def breed 
-  @breed
+  def breed 
+    @breed
   end
 end
