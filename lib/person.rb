5214dce7f4b2831502c3a43df88508a8cfa20d4c
Alexander Baker <alex.baker685@gmail.com>
2018-11-02 20:21:54 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,13 +1,13 @@
 class Person
 
-  def name =(persons_name)
+  def name= (persons_name)
     @name = persons_name
   end
 
   def name
     @name
   end
-  
+
   def job=(persons_job)
     @job = persons_job
   end
